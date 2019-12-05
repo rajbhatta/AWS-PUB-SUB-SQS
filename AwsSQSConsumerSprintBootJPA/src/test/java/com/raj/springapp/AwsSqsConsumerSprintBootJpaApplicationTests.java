425a04570eb567bd65f0918fb1f95e211152c766
rajbhatta <bhattaraj21@gmail.com>
2019-12-04 22:33:54 -0700

    Sprint Boot App consuming SQS message and writing to MYSQL database using Spring Data JPA

--- AwsSQSConsumerSprintBootJPA/src/test/java/com/raj/springapp/AwsSqsConsumerSprintBootJpaApplicationTests.java
+++ AwsSQSConsumerSprintBootJPA/src/test/java/com/raj/springapp/AwsSqsConsumerSprintBootJpaApplicationTests.java
@@ -1,12 +1,12 @@
 package com.raj.springapp;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AwsSqsConsumerSprintBootJpaApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
---
+++ sql-script-mysql.sql
@@ -0,0 +1,48 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.5.5-10.1.38-MariaDB : Database - awssqsdb
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`awssqsdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `awssqsdb`;
+
+/*Table structure for table `hibernate_sequence` */
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `hibernate_sequence` */
+
+insert  into `hibernate_sequence`(`next_val`) values (1);
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL,
+  `message` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `student` */
+
+insert  into `student`(`id`,`message`) values (1,'Hi I\'m excited to send message to AWS SQS');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
